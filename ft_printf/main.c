7fff49e39685b2ab7c8c2970551c576d733ac90f
Ricardo Mano Clemente Santos Dias <ridias@c2r2s7.42lisboa.com>
2025-11-17 12:28:01 +0000

    added further tests for my printf

--- ft_printf/main.c
+++ ft_printf/main.c
@@ -6,7 +6,7 @@
 /*   By: ridias <ridias@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/13 16:21:01 by ridias            #+#    #+#             */
-/*   Updated: 2025/11/14 00:08:41 by ridias           ###   ########.fr       */
+/*   Updated: 2025/11/17 12:17:09 by ridias           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int	main(void)
 
 	u_int_v = 4294967295;
 	str = "MARIA IS THE bEST";
-	ft_printf("The function woks no variables.\n");
+	/* ft_printf("The function woks no variables.\n");
 	ft_printf("The function woks no variables = \"%\".\n");
 	ft_printf("The function woks no variables = \"%%\".\n");
 	ft_printf("The function woks no variables = \"%%%\".\n");
@@ -35,6 +35,12 @@ int	main(void)
 	ft_printf("The function woks hexa x = \"%x\".\n", u_int_v);
 	ft_printf("The function woks hexa X = \"%X\".\n", u_int_v);
 	ft_printf("The function woks hexa x = \"%x\".\n", 1234567890);
-	ft_printf("The function woks hexa X = \"%X\".\n", 1234567890);
+	ft_printf("The function woks hexa X = \"%X\".\n", 1234567890); */
+	
+	ft_printf("The function woks hexa X = \"%y\".\n", 1234567890);
+	printf("The function woks hexa X = \"%y\".\n", 1234567890);
+	ft_printf("The function woks hexa X = \"%c\".\n", 1);
+	printf("The function woks hexa X = \"%c\".\n", 1);
+
 	return (0);
 }
