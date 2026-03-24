47f52d965005c276a233ca6b6688aeb8eadd2839
Ri787878 <ricardosdias@live.com>
2026-03-24 16:22:56 +0000

    cleaned out ex7

--- python-piscine/m_00/ex7/ft_seed_inventory.py
+++ python-piscine/m_00/ex7/ft_seed_inventory.py
@@ -6,7 +6,7 @@
 #    By: ridias <ridias@student.42.fr>              +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2026/03/19 18:14:50 by ridias            #+#    #+#              #
-#    Updated: 2026/03/24 16:17:25 by ridias           ###   ########.fr        #
+#    Updated: 2026/03/24 16:19:20 by ridias           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -19,11 +19,3 @@ def ft_seed_inventory(seed_type: str, quantity: int, unit: str) -> None:
 		print(f"{seed_type.capitalize()} seeds: covers {quantity} square meters")
 	else:
 		print(f"Unknown unit type")
-
-if __name__ == "__main__":
-	ft_seed_inventory("tomato", 15, "packets")
-	print()
-	ft_seed_inventory("carrot", 8, "grams")
-	print()
-	ft_seed_inventory("lettuce", 12, "area")
-	print()
