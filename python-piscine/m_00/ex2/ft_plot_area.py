e7efcd50cbb011733513d6d06cf9afd0cd1f9b5f
Ri787878 <ricardosdias@live.com>
2026-04-08 12:34:07 +0100

    fixed small mispelling error in file name

--- python-piscine/m_00/ex2/ft_plot_area.py
+++ python-piscine/m_00/ex2/ft_plot_area.py
@@ -1,7 +1,5 @@
 def ft_plot_area() -> None:
     lenght = int(input("Enter lengh: "))
     width = int(input("Enter width: "))
-    print(lenght)
-    print(width)
     area = int(lenght * width)
     print(f"Plot area: {area}")
