f16e456cb68fce78e0b958a0cd9d689482d8a09e
Ricardo Mano Clemente Santos Dias <ridias@c1r5s1.42lisboa.com>
2026-04-20 14:22:52 +0100

    fixed tiny bug on a default message in ex3 m_02

--- python-piscine/m_02/ex3/ft_custom_errors.py
+++ python-piscine/m_02/ex3/ft_custom_errors.py
@@ -1,17 +1,17 @@
 class GardenError(Exception):
-    def __init__(self, message):
+    def __init__(self, message="Unknown plant error"):
         self.message = message
         super().__init__(message)
 
 
 class PlantError(GardenError):
-    def __init__(self, message):
+    def __init__(self, message="Unknown plant error"):
         self.message = message
         super().__init__(message)
 
 
 class WaterError(GardenError):
-    def __init__(self, message):
+    def __init__(self, message="Unknown plant error"):
         self.message = message
         super().__init__(message)
 
