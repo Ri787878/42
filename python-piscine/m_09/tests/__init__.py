caf1761b15ee068c696853089f34d85de95bc024
Ricardo Mano Clemente Santos Dias <ridias@c2r4s1.42lisboa.com>
2026-06-04 14:47:26 +0100

    added func to test with tester

---
+++ python-piscine/m_09/tests/__init__.py
@@ -0,0 +1,13 @@
+from .data_generator import (
+    AlienContactGenerator as AlienContactGenerator,
+    CrewMissionGenerator as CrewMissionGenerator,
+    DataConfig as DataConfig,
+    SpaceStationGenerator as SpaceStationGenerator,
+)
+
+__all__ = [
+    "DataConfig",
+    "SpaceStationGenerator",
+    "AlienContactGenerator",
+    "CrewMissionGenerator",
+]
