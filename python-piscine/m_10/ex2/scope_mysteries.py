cdbbabbefbb9eeee2ac08287534db84a278ac373
Ricardo Mano Clemente Santos Dias <ridias@c2r9s3.42lisboa.com>
2026-06-09 16:52:14 +0100

    done ex3

--- python-piscine/m_10/ex2/scope_mysteries.py
+++ python-piscine/m_10/ex2/scope_mysteries.py
@@ -1,7 +1,4 @@
 from collections.abc import Callable
-"""
-def memory_vault() -> dict[str, Callable]:
-"""
 
 
 def mage_counter() -> Callable:
@@ -22,12 +19,30 @@ def spell_accumulator(initial_power: int) -> Callable:
     return accumulate
 
 
+def enchantment_factory(enchantment_type) -> Callable:
+    def enchant_description(item_name) -> str:
+        return (f"{enchantment_type} {item_name}")
+    return enchant_description
+
+
+def memory_vault() -> dict[str, Callable]:
+    storage: dict = {}
+
+    def store(key: str, value: object) -> None:
+        storage[key] = value
+    
+    def recall(key: str) -> object:
+        return storage.get(key, "Memory not found")
+    
+    return {"store": store, "recall": recall}
+
+
 def test_mage_counter() -> None:
     print("--- Test mage counter ---\nTest 1 counter: ", end="")
     count1 = mage_counter()
     for _ in range(10):
         print(count1(), end=" ")
-    print("\nTest 2 counters simultaneausly", end="")
+    print("\nTest 2 counters simultaneausly: ", end="")
     for _ in range(10):
         count2 = mage_counter()
         print(count2(), end=" ")
@@ -43,14 +58,34 @@ def test_spell_accumulator() -> None:
 
 
 def test_factory() -> None:
-    pass
+    print("--- Test enchantment factory ---")
+    fac = enchantment_factory("Flaming")
+    print("The enhancement is 'Flaming'\n")
+    print("First item is: ", end="")
+    print(fac("Sword"))
+    print("Second item is: ", end="")
+    print(fac("Spear"))
+    print("Third item is: ", end="")
+    print(fac("Shield"))
+    print()
+
+
+def test_vault() -> None:
+    print("--- Test Memory Vault ---\n")
+    vault = memory_vault()
+
+    vault["store"]("secret", 42)
+    print("Store 'secret' = 42")
+    print(f"Recall 'secret': {vault['recall']('secret')}")
+    print(f"Recall 'unknown': {vault['recall']('unknown')}")
 
 
 if __name__ == "__main__":
     try:
-        # test_mage_counter()
-        # test_spell_accumulator()
+        test_mage_counter()
+        test_spell_accumulator()
         test_factory()
+        test_vault()
         pass
     except Exception as e:
         print(e)
